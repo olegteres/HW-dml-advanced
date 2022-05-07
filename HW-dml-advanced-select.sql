fb7740f7e733f6e6be1a6d7193ce437ebc94f042
olegteres <happydoggy@yandex.ru>
2022-05-07 23:13:02 +0300

    correct-select

--- HW-dml-advanced-select.sql
+++ HW-dml-advanced-select.sql
@@ -1,23 +1,19 @@
---количество исполнителей в каждом жанре
 select g.name, count(s.name) from genre as g
 	left join genre_singer as gs on g.id = gs.genre_id
 	left join singer as s on gs.singer_id = s.id
 	group by g.name
 	order by count(s.id) desc;
 
---количество треков, вошедших в альбомы 2019-2020 годов
 select t.name, a.release_year from album as a
 	left join track as t on t.album_id = a.id
 	where (a.release_year >= 2019) and (a.release_year <= 2020);
 	
---средняя продолжительность треков по каждому альбому
 select a.name, AVG(t.length_seconds)
 	from album as a
 	left join track as t on t.album_id = a.id
 	group by a.name
 	order by AVG(t.length_seconds);
 
---все исполнители, которые не выпустили альбомы в 2020 году
 select distinct s.name
 	from singer as s
 	where s.name not in (
@@ -28,7 +24,6 @@ select distinct s.name
     	where a.release_year = 2020)
 	order by s.name;
 
---названия сборников, в которых присутствует конкретный исполнитель (выберите сами)
 select distinct c.name from collection as c
 	left join collection_track as ct on c.id = ct.collection_id
 	left join track as t on t.id = ct.track_id
@@ -38,7 +33,6 @@ select distinct c.name from collection as c
 	where s.name like '%%Philipp Kirkorov%%'
 	order by c.name;
 
---название альбомов, в которых присутствуют исполнители более 1 жанра
 select a.name from album as a
 	left join singer_album as sa on a.id = sa.album_id
 	left join singer as s on s.id = sa.singer_id
@@ -48,12 +42,10 @@ select a.name from album as a
 	having count(distinct g.name) > 1
 	order by a.name;
 
---наименование треков, которые не входят в сборники
 select t.name from track as t
 	left join collection_track as ct on t.id = ct.track_id
 	where ct.track_id is NULL;
 
---исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько)
 select s.name, t.length_seconds from track as t
 	left join album as a on a.id = t.album_id
 	left join singer_album as sa on sa.album_id = a.id
@@ -62,7 +54,6 @@ select s.name, t.length_seconds from track as t
 	having t.length_seconds = (select min(length_seconds) from track)
 	order by s.name;
 
---название альбомов, содержащих наименьшее количество треков
 select distinct a.name from album as a
 	left join track as t on t.album_id = a.id
 	where t.album_id in (
